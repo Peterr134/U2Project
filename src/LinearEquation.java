d3819356e8e7713eb82e35c1e72569d1e84eb8a8
Peterr134 <peterr134@nycstudents.net>
2023-10-19 09:35:44 -0400

    ABC

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+
+    public LinearEquation (double x1, double y1, double x2, double y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double roundHundredth(double input){
+        return ((int)(input*100))/100;
+    }
+
+    public String getPointCoordinates(){
+        return("The coordinates of point 1 are (" + x1 + ", " + y1 + ") and the coordinates of point 2 are (" + x1 + ", " + y2 + ")");
+    }
+
+    public double getSlope(){
+        return roundHundredth((y2-y1)/(x2-x1));
+    }
+
+    public double getYIntercept(){
+        return roundHundredth(y1-getSlope()*x1);
+    }
+
+    public String getEquation(){
+        if(getSlope()%1==0) {
+            if(getSlope()==1){
+                //slope is 1
+                if(getYIntercept()==0) {
+                    return ("y = x " + getYIntercept());
+                }else{
+                    return ("y = x ");
+                }
+            }else if (getSlope()==-1) {
+                //slope is -1
+                if(getYIntercept()==0) {
+                    return ("y=-x");
+                }else {
+                    return ("y = -x " + getYIntercept());
+                }
+            }else if (getSlope() == 0){
+                //slope is 0
+                return("y= " + getYIntercept());
+            }else{
+                //slope is a whole number, not 1, 0, or -1
+                if(getYIntercept()==0) {
+                    return "y = " + ((int) getSlope()) + "x + " + getYIntercept();
+                }else{
+                    return "y = " + ((int) getSlope()) + "x";
+                }
+            }
+        }else{
+            if(getSlope()<1) {
+                //slope is in fraction form, and negative
+                if(getYIntercept()==0) {
+                    return "y = " + "-" + Math.abs(y2 - y1) + "/" + Math.abs(x2 - x1) + "x";
+                }else{
+                    return "y = " + "-" + Math.abs(y2 - y1) + "/" + Math.abs(x2 - x1) + "x + " + getYIntercept();
+                }
+            }else{
+                //slope is in fraction form, and positive
+                if(getYIntercept()==0) {
+                    return "y = " + Math.abs(y2 - y1) + "/" + Math.abs(x2 - x1) + "x";
+                }else{
+                    return "y = " + Math.abs(y2 - y1) + "/" + Math.abs(x2 - x1) + "x + " + getYIntercept();
+                }
+            }
+        }
+    }
+
+    public double getDistance(){
+        return roundHundredth(Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
+    }
+
+    public String lineInfo(){
+        return(getPointCoordinates() + "\nThe equation of the line is " + getEquation() + "\nThe slope of the line is " + getSlope() + "\nThe y intercept is " + getYIntercept() + "\nThe points are " + getDistance() + " apart");
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome! Please input coordinates in this format: (x,y)");
+        System.out.print("Enter coordinate 1: ");
+        String coord1 = scanner.nextLine();
+        double x1 = Double.parseDouble(coord1.substring(1,coord1.indexOf(",")));
+        double y1 = Double.parseDouble(coord1.substring(coord1.indexOf(",")+1,coord1.indexOf(")")));
+        System.out.print("Enter coordinate 2: ");
+        String coord2 = scanner.nextLine();
+        double x2 = Double.parseDouble(coord2.substring(1,coord2.indexOf(",")));
+        double y2 = Double.parseDouble(coord2.substring(coord2.indexOf(",")+1,coord2.indexOf(")")));
+        System.out.println();
+        LinearEquation lineq = new LinearEquation(x1,y1,x2,y2);
+        System.out.println(lineq.lineInfo());
+        System.out.println();
+        System.out.print("Enter a value for X: ");
+        x1 = scanner.nextDouble();
+        System.out.println("This point on the line is (" + x1 + ", " + (x1*lineq.getSlope()+ lineq.getYIntercept()) + ")");
+    }
 }
